f07a7f141184ddbba6618f1a77e716ac280339fa
gilberto dos Anjos <gilbertodosanjos@gmail.com>
2023-05-24 11:54:02 -0300

    aula 29 NgClass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,10 @@ AfterViewChecked
 @Component({
   selector: 'app-root',
 template:`
-  <!--<app-diretivas-estruturais>  
-  <router-outlet></router-outlet>-->
+  <!--<app-diretivas-estruturais>  -->
   
+  <app-diretivas-atributos></app-diretivas-atributos>
+  <router-outlet></router-outlet>
   `
 })
 export class AppComponent implements OnInit {
--- src/app/diretivas-atributos/diretivas-atributos.component.ts
+++ src/app/diretivas-atributos/diretivas-atributos.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-diretivas-atributos',
   templateUrl: './diretivas-atributos.component.html',
   styleUrls: ['./diretivas-atributos.component.scss']
 })
-export class DiretivasAtributosComponent {
+export class DiretivasAtributosComponent implements OnInit {
+  
+  public valor:boolean = false;
+  constructor(){}
 
+  ngOnInit(): void {
+    setInterval(()=>{
+      if(this.valor){
+        this.valor = false;
+      }else{
+        this.valor=true;
+      }
+    },2000);
+  }
 }
